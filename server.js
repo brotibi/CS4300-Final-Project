51a10ce94780b2ae0b144f2ad591d8ccf49f06c5
Mobolaji Rotibi <brotibi@gmail.com>
2020-12-14 23:45:41 -0500

    finished???

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.get('/index.html', (req, res) => {
     res.sendFile('index.html');
 });
 
-
-app.listen(process.env.PORT || 3000, '127.0.0.1');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
